36de30f46962fd5c5f8afbee5e832d57d1566653
eminbasbayan <emin.basbayan@icloud.com>
2024-05-08 14:00:06 +0300

    react component'e css dosyası ekleme

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,14 +1,16 @@
+import "./ProductCard.css";
+
 function ProductCard() {
   return (
     <div className="product-card">
       <img
         src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
         alt="çanta görseli"
-        width={100}
-        height={100}
       />
-      <strong>Title</strong>
-      <span>100₺</span>
+      <div className="product-info">
+        <strong>Title</strong>
+        <span>100₺</span>
+      </div>
     </div>
   );
 }
