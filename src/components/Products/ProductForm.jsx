bc998630ad7df9e0b4007f77082706717962908c
eminbasbayan <emin.basbayan@icloud.com>
2024-05-09 13:42:26 +0300

    onChange event

--- src/components/Products/ProductForm.jsx
+++ src/components/Products/ProductForm.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 function ProductForm() {
+  const [image, setImage] = useState("");
+
+  function handleChange(event) {
+    setImage(event.target.value);
+  }
+
   return (
     <form>
       <div className="mb-3">
         <label className="form-label">Product Image</label>
-        <input type="text" className="form-control" />
+        <input type="text" className="form-control" onChange={handleChange} />
       </div>
+      {image}
       <div className="mb-3">
         <label className="form-label">Product Title</label>
         <input type="text" className="form-control" />
