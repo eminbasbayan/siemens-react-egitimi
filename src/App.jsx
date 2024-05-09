762f31fac3b868b67a5abf029569dfd64b8b19f7
eminbasbayan <emin.basbayan@icloud.com>
2024-05-09 10:31:35 +0300

    eventlerle çalışmak

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import Products from "./components/Products/Products";
+// import Products from "./components/Products/Products";
 
 function App() {
+  let products = [{ id: 1, name: "Çanta" }];
+  console.log(products);
+  
+  function handleClick() {
+    console.log("run!");
+  }
+
+  function addNewProduct(newProduct) {
+    products = [...products, newProduct];
+    console.log(products);
+  }
+
   return (
     <div className="app">
-      <Products />
+      <button onClick={handleClick}>Sepete Ekle</button>
+      <button onClick={() => addNewProduct({ id: 2, name: "Çorap" })}>
+        Yeni Ürün Ekle
+      </button>
     </div>
   );
 }
