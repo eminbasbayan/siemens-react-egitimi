3376f99736fd91156eb1b3f4c8073e3837a09be3
eminbasbayan <emin.basbayan@icloud.com>
2024-05-09 13:48:05 +0300

    product form düzenlendi

--- src/components/Products/ProductForm.jsx
+++ src/components/Products/ProductForm.jsx
@@ -2,28 +2,52 @@ import { useState } from "react";
 
 function ProductForm() {
   const [image, setImage] = useState("");
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState(0);
 
-  function handleChange(event) {
+  function handleImageChange(event) {
     setImage(event.target.value);
+  }
+
+  function handleTitleChange(event) {
+    setTitle(event.target.value);
+  }
+
+  function handlePriceChange(event) {
+    setPrice(event.target.value);
   }
 
   return (
     <form>
       <div className="mb-3">
         <label className="form-label">Product Image</label>
-        <input type="text" className="form-control" onChange={handleChange} />
+        <input
+          type="text"
+          className="form-control"
+          onChange={handleImageChange}
+        />
+        {image}
       </div>
-      {image}
       <div className="mb-3">
         <label className="form-label">Product Title</label>
-        <input type="text" className="form-control" />
+        <input
+          type="text"
+          className="form-control"
+          onChange={handleTitleChange}
+        />
+        {title}
       </div>
       <div className="mb-3">
         <label className="form-label">Product Price</label>
-        <input type="number" className="form-control" />
+        <input
+          type="number"
+          className="form-control"
+          onChange={handlePriceChange}
+        />
+        {price}
       </div>
       <button type="submit" className="btn btn-primary">
-        Submit
+        Yeni Ürün Ekle
       </button>
     </form>
   );
