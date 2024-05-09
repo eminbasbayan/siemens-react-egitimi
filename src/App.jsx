74a513212717ba85ad973833b9a6160495cb23c3
eminbasbayan <emin.basbayan@icloud.com>
2024-05-09 16:27:33 +0300

    useEffect kullanımı

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
-import Products from "./components/Products/Products";
-import 'react-toastify/dist/ReactToastify.css';
+// import Products from "./components/Products/Products";
+import "react-toastify/dist/ReactToastify.css";
+// import { WebSocket } from "vite";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState("Çorap");
+  
+  useEffect(() => {
+    console.log("İlk yüklendiğinde çalıştı!");
+  }, []);
+
+  useEffect(() => {
+    console.log("İlk yüklendiğinde ve güncellendiğinde çalıştı!");
+  }, [count]);
+
+  useEffect(() => {
+    
+    // component ekrandan kaldırıldığı çalıştı!
+
+    // clean-up function
+    return () => {
+      console.log("Ekrandan kaldırıldığında");
+      // WebSocket.CLOSED();
+    };
+  }, []);
+
   return (
     <div className="app">
-      <Products />
+      <button onClick={() => setCount(count + 1)}>Arttır</button>
+      <p>{count}</p>
+      <button onClick={() => setTitle("Şapla")}>Title Güncelle</button>
+      {title}
+      {/* <Products /> */}
       <ToastContainer />
     </div>
   );
