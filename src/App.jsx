092b34013ac4a96a955f72f494d3f3f3dcf9cf24
eminbasbayan <emin.basbayan@icloud.com>
2024-05-10 11:09:34 +0300

    ilk sayfamız

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import Products from "./components/Products/Products";
-import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Layout/Header";
+import HomePage from "./pages/HomePage";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+  ]);
+
   return (
     <div className="app">
       <Header />
       <section className="content">
-        <Products />
+        <RouterProvider router={router} />
       </section>
       <ToastContainer />
     </div>
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,33 @@
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1>Ana Sayfaya Hoşgeldiniz!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed suscipit
+        eligendi quaerat nulla harum nobis saepe, atque soluta. Tempore maxime
+        vel rerum? Tempore non delectus provident nemo sit quia similique alias
+        magnam voluptatibus iure asperiores iste cum labore voluptatum, possimus
+        odit nesciunt accusamus? Ipsa perferendis libero aspernatur velit et
+        unde voluptatibus tempore iste modi, consequuntur quibusdam qui mollitia
+        temporibus dolorem, saepe delectus error repudiandae numquam voluptas,
+        laboriosam molestias explicabo veritatis. Distinctio adipisci itaque ut
+        quidem ullam voluptatibus, in illo hic nam beatae recusandae, commodi ea
+        magni alias, repellendus dicta officiis quae eveniet possimus tenetur
+        aperiam fugit qui molestiae. Eos illum harum quisquam quod sequi at
+        perferendis natus amet voluptates pariatur iure nihil esse, ipsam
+        sapiente provident similique excepturi, in molestiae cum, cupiditate
+        magni perspiciatis! Rem autem molestiae earum id, molestias deleniti. In
+        cumque eligendi repellat, expedita corrupti exercitationem ducimus vitae
+        reprehenderit culpa delectus dolorem illo totam optio sequi, eos eum ad,
+        suscipit doloribus labore doloremque! Magnam veniam officiis quis autem
+        provident nobis animi porro illum, cupiditate, accusantium ipsum tempore
+        quaerat error corporis in facere perspiciatis modi quod, repellat
+        repellendus possimus! Nihil omnis possimus laudantium libero, sunt saepe
+        reiciendis quod quibusdam veritatis, in ad consequatur, voluptate iure
+        quis recusandae deleniti sint?
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
